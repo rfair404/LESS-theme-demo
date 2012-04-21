66544ad6d858086cbbd7ed27e04e0f6d6b3760d9
Russell Fair <rfair404@gmail.com>
2012-04-21 01:17:24 -0400

    clean up less

--- functions.php
+++ functions.php
@@ -2,9 +2,16 @@
 
 define ('THEME_URL', get_stylesheet_directory_uri() );
 
-add_action('wp_head', 'ld_janky_way_less');
+add_action('wp_head', 'ltd_janky_way_less');
 
-function ld_janky_way_less() { ?>
+function ltd_janky_way_less() { ?>
 	<link rel="stylesheet/less" type="text/css" href="<?php echo THEME_URL; ?>/less/example.less">
 	<script src="<?php echo THEME_URL; ?>/js/less-1.2.0.min.js" type="text/javascript"></script>
 <?php } ?>
+
+<?php
+add_action('wp_print_styles', 'ltd_okay_way_2011css');
+
+function ltd_okay_way_2011css() {
+	wp_enqueue_style('twentyeleven-css', get_bloginfo( 'template_url' ) . '/style.css' , array(), '007');
+} 
